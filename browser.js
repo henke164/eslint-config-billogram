851b2c25ad4267dd54a04b869fa23ae1f5e2ae75
Mattias Rydengren <mattias.rydengren@billogram.com>
2016-05-27 14:56:52 +0200

    Enable experimental object rest spread.

--- browser.js
+++ browser.js
@@ -1,7 +1,10 @@
 module.exports = {
     'parserOptions': {
         "ecmaVersion": 6,
-        "sourceType": "module"
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true
+        }
     },
     'env': {
         'browser': true,
